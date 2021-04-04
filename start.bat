9f6ba601e5043f126173f8e63904be0463106601
MunnaBhai-01 <80153720+MunnaBhai-01@users.noreply.github.com>
2021-04-04 14:03:19 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Saitama Robot
-:: Enables virtual env mode and then starts saitama
-env\scripts\activate.bat && py -m SaitamaRobot
+TITLE SarcasticLoverRobot
+:: Enables virtual env mode and then starts SarcasticLover
+env\scripts\activate.bat && py -m SarcasticLoverRobot
